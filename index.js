6a6cb3f5066231e11d89182bf99cac0c45b45ebb
sivaabhay123-blip <sivaabhay123@gmail.com>
2026-04-05 12:39:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,6 @@
-const http = require('http');
-
-const port = 3000;
-
 http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-
-  res.end(`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>My Site</title>
-      </head>
-      <body style="text-align:center; font-family:Arial; margin-top:50px;">
-        <h1>✨ My Site is Working ✨</h1>
-        <p>Welcome to my website!</p>
-      </body>
-    </html>
-  `);
-
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('MY SITE IS WORKING...');
 }).listen(port, () => {
-  console.log("Server running at http://localhost:3000");
+  console.log(`Server running at http://localhost:${port}`);
 });

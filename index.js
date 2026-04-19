01f13835550c28127cafb217f16b7ecca4e14680
sivaabhay123-blip <sivaabhay123@gmail.com>
2026-04-19 10:07:42 +0530

    Update and rename index.js to index.html

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const port = process.env.PORT || 3000;
-
-http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('MY SITE IS WORKING');
-}).listen(port);

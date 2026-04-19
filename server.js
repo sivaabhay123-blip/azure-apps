f487137238467175b1892ac11069b549faf740d8
sivaabhay123-blip <sivaabhay123@gmail.com>
2026-04-19 10:29:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-app.use(express.static(__dirname));
+const express = require("express");
+const path = require("path");
 
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// serve static files
+app.use(express.static(path.join(__dirname, "public")));
+
+// homepage
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
